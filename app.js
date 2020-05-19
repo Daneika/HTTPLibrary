11118217ed2417f8dcdc1ae3e3897f79fa180942
meg <megan.daneika@gmail.com>
2020-05-19 10:53:51 +0100

    Add a POST request

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const http = new EasyHTTP
 
-// Get users
-http.get("https://jsonplaceholder.typicode.com/users")
+// // Get users
+// http.get("https://jsonplaceholder.typicode.com/users")
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+// user data
+const data = {
+    name: "John Smith",
+    username: "johnsmith",
+    email: "jsmith@gmail.com"
+}
+
+// Create post
+http.post("https://jsonplaceholder.typicode.com/users", data)
 .then(data => console.log(data))
 .catch(err => console.log(err))
